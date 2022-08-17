f19a70da26c4d7e9ed3988b7c7f65600fac23ba5
Jake Butler <JakeButler.student@careerdevs.com>
2022-08-16 21:48:22 -0400

    Added Post, Put, Delete mapping to UserController

--- src/main/java/com/careerdevs/jsonplaceholder/controllers/UserController.java
+++ src/main/java/com/careerdevs/jsonplaceholder/controllers/UserController.java
@@ -1,11 +1,10 @@
 package com.careerdevs.jsonplaceholder.controllers;
 
 import com.careerdevs.jsonplaceholder.models.UserModel;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
@@ -72,46 +71,90 @@ public class UserController {
 
     }
 
-//    @GetMapping("/photos")
-//    public Object photoHandler(RestTemplate restTemplate) {
-//        String photoEndpoint = jsonPlaceholderEndpoint;
-//        photoEndpoint += "photos";
-//        return restTemplate.getForObject(photoEndpoint, Object.class);
-//    }
-//
-//    @GetMapping("/posts")
-//    public Object postsHandler(RestTemplate restTemplate) {
-//        String postsEndpoint = jsonPlaceholderEndpoint;
-//        postsEndpoint += "posts";
-//        return restTemplate.getForObject(postsEndpoint, Object.class);
-//    }
-//
-//    @GetMapping("/comments")
-//    public Object commentsHandler(RestTemplate restTemplate) {
-//        String commentsEndpoint = jsonPlaceholderEndpoint;
-//        commentsEndpoint += "comments";
-//        return restTemplate.getForObject(commentsEndpoint, Object.class);
-//    }
-//
-//    @GetMapping("/albums")
-//    public Object albumHandler(RestTemplate restTemplate) {
-//        String albumsEndpoint = jsonPlaceholderEndpoint;
-//        albumsEndpoint += "albums";
-//        return restTemplate.getForObject(albumsEndpoint, Object.class);
-//    }
-//
-//    @GetMapping("/users")
-//    public Object userHandler(RestTemplate restTemplate) {
-//        String userEndpoint = jsonPlaceholderEndpoint;
-//        userEndpoint += "users";
-//        return restTemplate.getForObject(userEndpoint, Object.class);
-//    }
-//
-//    @GetMapping("/todos")
-//    public Object todoHandler(RestTemplate restTemplate) {
-//        String todoEndpoint = jsonPlaceholderEndpoint;
-//        todoEndpoint += "todos";
-//        return restTemplate.getForObject(todoEndpoint, Object.class);
-//    }
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<?> deleteUserById(RestTemplate restTemplate, @PathVariable String id) {
+        try {
+
+            // throws a number format exception if id is not int
+            Integer.parseInt(id);
+
+            System.out.println("Getting user with id: " + id);
+
+            String url = jsonPHUserEndpoint + "/" + id;
+
+            restTemplate.getForObject(url, UserModel.class);
+
+            restTemplate.delete(url);
+
+            return ResponseEntity.ok("User with ID " + id + " has been deleted.");
+
+        } catch (NumberFormatException e) {
+
+            return ResponseEntity.status(400).body("ID " + id + " was not found in the database. Must be a whole number.");
+
+        } catch (HttpClientErrorException.NotFound e) {
+
+            return ResponseEntity.status(404).body("User Not Found With ID: " + id);
+
+        } catch (Exception e) {
+
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    // POST localhost:8080/api/users
+    @PostMapping("/")
+    public ResponseEntity<?> createNewUser(RestTemplate restTemplate, @RequestBody UserModel newUser) {
+        try {
+
+            //TODO: User data validation
+
+            UserModel createdUser = restTemplate.postForObject(jsonPHUserEndpoint, newUser, UserModel.class);
+
+
+            return ResponseEntity.ok(createdUser);
+
+        }  catch (Exception e) {
+
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    // PUT localhost:8080/api/users
+    @PutMapping("/id/{id}")
+    public ResponseEntity<?> updateNewUser(RestTemplate restTemplate, @RequestBody UserModel updateUserData, @PathVariable String id) {
+        try {
+
+            Integer.parseInt(id);
+
+            String url = jsonPHUserEndpoint + "/" + id;
+
+            //TODO: User data validation
+            HttpEntity<UserModel> reqEntity = new HttpEntity<>(updateUserData);
+
+//            restTemplate.put(url, updateUserData, UserModel.class);
+            ResponseEntity<UserModel> jphRes =  restTemplate.exchange(url, HttpMethod.PUT, reqEntity ,UserModel.class);
+
+            return ResponseEntity.ok(jphRes.getBody());
+
+        }  catch (NumberFormatException e) {
+
+            return ResponseEntity.status(400).body("ID " + id + " was not found in the database. Must be a whole number.");
+
+        } catch (HttpClientErrorException.NotFound e) {
+
+            return ResponseEntity.status(404).body("User Not Found With ID: " + id);
+
+        } catch (Exception e) {
+
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
 
 }
