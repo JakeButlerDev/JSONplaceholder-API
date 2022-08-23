80d444674f2d5a5a781c3121289b6f0e54660268
Jake Butler <JakeButler.student@careerdevs.com>
2022-08-23 17:48:31 -0400

    Added Post, Put, Delete mapping to all controllers

--- src/main/java/com/careerdevs/jsonplaceholder/controllers/PostController.java
+++ src/main/java/com/careerdevs/jsonplaceholder/controllers/PostController.java
@@ -1,11 +1,11 @@
 package com.careerdevs.jsonplaceholder.controllers;
 
 import com.careerdevs.jsonplaceholder.models.PostModel;
+import com.careerdevs.jsonplaceholder.models.UserModel;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
@@ -53,6 +53,91 @@ public class PostController {
         } catch (HttpClientErrorException.NotFound e) {
 
             return ResponseEntity.status(404).body("Post Not Found With ID: " + id);
+
+        } catch (Exception e) {
+
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<?> deleteUserById(RestTemplate restTemplate, @PathVariable String id) {
+        try {
+
+            // throws a number format exception if id is not int
+            Integer.parseInt(id);
+
+            System.out.println("Getting user with id: " + id);
+
+            String url = jsonPHPostsEndpoint + "/" + id;
+
+            restTemplate.getForObject(url, UserModel.class);
+
+            restTemplate.delete(url);
+
+            return ResponseEntity.ok("User with ID " + id + " has been deleted.");
+
+        } catch (NumberFormatException e) {
+
+            return ResponseEntity.status(400).body("ID " + id + " was not found in the database. Must be a whole number.");
+
+        } catch (HttpClientErrorException.NotFound e) {
+
+            return ResponseEntity.status(404).body("User Not Found With ID: " + id);
+
+        } catch (Exception e) {
+
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    // POST localhost:8080/api/users
+    @PostMapping("/")
+    public ResponseEntity<?> createNewUser(RestTemplate restTemplate, @RequestBody UserModel newUser) {
+        try {
+
+            //TODO: User data validation
+
+            UserModel createdUser = restTemplate.postForObject(jsonPHPostsEndpoint, newUser, UserModel.class);
+
+
+            return ResponseEntity.ok(createdUser);
+
+        }  catch (Exception e) {
+
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    // PUT localhost:8080/api/users
+    @PutMapping("/id/{id}")
+    public ResponseEntity<?> updateNewUser(RestTemplate restTemplate, @RequestBody UserModel updateUserData, @PathVariable String id) {
+        try {
+
+            Integer.parseInt(id);
+
+            String url = jsonPHPostsEndpoint + "/" + id;
+
+            //TODO: User data validation
+            HttpEntity<UserModel> reqEntity = new HttpEntity<>(updateUserData);
+
+//            restTemplate.put(url, updateUserData, UserModel.class);
+            ResponseEntity<UserModel> jphRes =  restTemplate.exchange(url, HttpMethod.PUT, reqEntity ,UserModel.class);
+
+            return ResponseEntity.ok(jphRes.getBody());
+
+        }  catch (NumberFormatException e) {
+
+            return ResponseEntity.status(400).body("ID " + id + " was not found in the database. Must be a whole number.");
+
+        } catch (HttpClientErrorException.NotFound e) {
+
+            return ResponseEntity.status(404).body("User Not Found With ID: " + id);
 
         } catch (Exception e) {
 
